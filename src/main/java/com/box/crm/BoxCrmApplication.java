258f9d49cdc71f8f9faabe4708fa53b3e862deb6
syz <sunyizhuo@box-hill.com.cn>
2020-03-04 22:55:38 +0800

    启动类扫描dao

--- src/main/java/com/box/crm/BoxCrmApplication.java
+++ src/main/java/com/box/crm/BoxCrmApplication.java
@@ -1,5 +1,6 @@
 package com.box.crm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication(scanBasePackages= {"com.box.**"})
 @EnableAsync
 @ServletComponentScan
+@MapperScan("com.box.**.dao")
 public class BoxCrmApplication {
 
 	public static void main(String[] args) {
